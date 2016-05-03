a0dd10e373608fdb6a4ab746ac93ac5aa059818f
aik <aik>
2016-05-03 18:22:05 +0100

    update code

--- resource/luxe_beatdetection/src/Main.hx
+++ resource/luxe_beatdetection/src/Main.hx
@@ -1,6 +1,8 @@
 package;
 
 import luxe.Input;
+import luxe.Screen;
+import luxe.Vector;
 
 // basic sprite
 import luxe.Sprite;
@@ -13,13 +15,21 @@ class Main extends luxe.Game
 	override function ready() 
 	{
 		// create an asset here
-		
+		m_centerPiece = new Sprite({
+			name: "beat",
+			pos: Luxe.screen.mid,
+		size: new Vector(128,128)});
 	}
 
 	override function onkeyup(e:KeyEvent) 
 	{
 		if(e.keycode == Key.escape)
 			Luxe.shutdown();
+			
+		if (e.keycode == Key.space)
+		{
+			m_centerPiece.destroy();
+		}
 	}
 
 	override function update(dt:Float) 
