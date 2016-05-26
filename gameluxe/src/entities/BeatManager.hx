b6c67e9b5cbca66eaf49395c356e648e60de4922
aik6980 <aik6980>
2016-05-26 07:49:48 +0100

    wip beatmanager

--- gameluxe/src/entities/BeatManager.hx
+++ gameluxe/src/entities/BeatManager.hx
@@ -5,6 +5,7 @@ import luxe.Audio.AudioHandle;
 import luxe.Entity;
 import luxe.options.EntityOptions;
 import luxe.resource.Resource.AudioResource;
+import snow.api.buffers.Uint8Array;
 
 /**
  * ...
@@ -14,6 +15,13 @@ class BeatManager extends Entity
 {
 	private var music: AudioResource;
 	private var music_handle: luxe.Audio.AudioHandle;
+	
+	/// helpers
+	var audio_data_len = 0;
+	var audio_data : Uint8Array;
+	
+	/// constants
+	var 
 	
 	public function new(?_options:EntityOptions) 
 	{
@@ -39,6 +47,8 @@ class BeatManager extends Entity
 			trace("Rate: " + music.source.data.rate);
 			trace("Length: " + music.source.data.length);
 			
+			audio_data_len = music.source.data.length;
+			
 	 		//var s = "";
 			//for (i in 0...1024)
 			//{
@@ -47,6 +57,36 @@ class BeatManager extends Entity
 			//log(s);
 			
 			//Luxe.showConsole(true);
+			
+			process_audio();
 		});
+	}
+	
+	public function process_audio()
+	{
+		// Get Data
+		var music_instance = music.source.instance(music_handle);
+		
+		trace(music_instance);
+		
+		audio_data = new Uint8Array(audio_data_len);
+		var audio_data_query_result = new Array<Int>();
+		music_instance.data_get(audio_data, 0, audio_data_len, audio_data_query_result);
+		
+		// calculate instant energy
+		
+	}
+	
+	/// helpers
+	function energy(data:Uint8Array, offset:UInt, window:UInt): Int
+	{
+		var res = 0.0;
+		var end = Std.int(Math.min(offset + window, audio_data_len));
+		for (i in offset...end)
+		{
+			res += audio_data[i] * audio_data[i] / window; 
+		}
+		
+		return Std.int(res);
 	}
 }
