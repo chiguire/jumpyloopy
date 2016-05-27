8c5c4649e5f0c8ebfb5deb52e5ef8165a0ec4802
aik6980 <aik6980>
2016-05-27 09:01:24 +0100

    wip beat detector

--- gameluxe/src/entities/BeatManager.hx
+++ gameluxe/src/entities/BeatManager.hx
@@ -23,6 +23,11 @@ class BeatManager extends Entity
 	/// constants
 	var instant_interval = 1024;
 	
+	/// analysis
+	var energy1024 : Array<Float>; 	// instant energy
+	var energy44100 : Array<Float>; // local energy
+	var energy_peak: Array<Float>;
+	
 	public function new(?_options:EntityOptions) 
 	{
 		super(_options);
@@ -73,12 +78,24 @@ class BeatManager extends Entity
 		var audio_data_query_result = new Array<Int>();
 		music_instance.data_get(audio_data, 0, audio_data_len, audio_data_query_result);
 		
-		// calculate instant energy
+		// initialize inttermidiate buffers
+		energy1024 = new Array<Float>();
+		energy44100 = new Array<Float>();
 		
+		// calculate instant energy every 1024 samples, stored in the buffer
+		for ( i in 0...Std.int(audio_data_len / instant_interval))
+		{
+			var e = energy(audio_data, i * instant_interval, 4096); // 4096? why not using 1024
+			energy1024.push(e);
+		}
+		//trace(energy1024.length);
+		
+		// calculate local average energy
+		calculate_avg_local_energy();
 	}
 	
 	/// helpers
-	function energy(data:Uint8Array, offset:UInt, window:UInt): Int
+	function energy(data:Uint8Array, offset:UInt, window:UInt): Float
 	{
 		var res = 0.0;
 		var end = Std.int(Math.min(offset + window, audio_data_len));
@@ -87,6 +104,43 @@ class BeatManager extends Entity
 			res += audio_data[i] * audio_data[i] / window; 
 		}
 		
-		return Std.int(res);
+		return res;
+	}
+	
+	function calculate_avg_local_energy()
+	{
+		var sum = 0.0;
+		// calculation of the first second
+		// 43 came from 44100/1024
+		var num_objects = 43; 
+		for ( i in 0...num_objects )
+		{
+			sum += energy1024[i];
+		}
+		energy44100.push( sum / num_objects);
+		
+		// optimization, here we keep the sum and shift one step at the time
+		// so we can reduce amount of unnecessary computation
+		for ( i in 1...Std.int(audio_data_len / 1024) )
+		{
+			// [Note] I feel like this part of calculation will cause an array out of bounds at the end
+			// maybe the correct thing to do here is wrap the i index around
+			// esp. when the music is looping
+			var next_idx = (i + num_objects - 1) % energy1024.length;
+			var prev_idx = i - 1;
+			
+			// practically, what we do here is replaced the first item with the next item
+			// yielding the new sum for the next window
+			sum += energy1024[next_idx] - energy1024[prev_idx];
+			// then calculate this local average
+			energy44100.push(sum / num_objects);
+		}
+		
+		//trace(energy44100.length);
+	}
+	
+	function calculate_peak_energy()
+	{
+		
 	}
 }
