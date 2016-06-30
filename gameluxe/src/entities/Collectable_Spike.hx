5e20140fcca32e99cb47936a0f2698948305c82b
aik6980 <aik6980>
2016-06-30 07:58:45 +0100

    disable spike event for presentation

--- gameluxe/src/entities/Collectable_Spike.hx
+++ gameluxe/src/entities/Collectable_Spike.hx
@@ -17,7 +17,8 @@ class Collectable_Spike extends Collectable
 	override function onCollisionEnter(player:Avatar):Void 
 	{
 		//Do fun stuff.
-		Luxe.events.fire("kill_player", {msg:"Massive SPIKES!"});
+		// A bit of a game breaker, commenting out for now [Aik]
+		//Luxe.events.fire("kill_player", {msg:"Massive SPIKES!"});
 		super.onCollisionEnter(player);
 	}
 }
