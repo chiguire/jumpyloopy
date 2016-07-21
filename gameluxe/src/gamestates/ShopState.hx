357e69845f4407fdc280bf6cf6f10aa99ee05521
Simon Mann <simonjmann@gmail.com>
2016-07-21 22:44:03 +0100

    Store looks a bit better now.

--- gameluxe/src/gamestates/ShopState.hx
+++ gameluxe/src/gamestates/ShopState.hx
@@ -25,6 +25,8 @@ import mint.Scroll;
 import ui.MintGridPanel;
 import ui.MintImageButton;
 import ui.MintImageButton_Store;
+import ui.MintLabel;
+import mint.types.Types.TextAlign;
 
 /**
  * ...
@@ -38,7 +40,7 @@ class ShopState extends State
 	var parcel : Parcel;
 	var change_to : String = "";
 	var canvas : Canvas;
-	var coins_text : Label;
+	var coins_text : MintLabel;
 	
 	private var equipped_character_button : MintImageButton_Store;
 	private var equipped_background_button : MintImageButton_Store;
@@ -115,50 +117,56 @@ class ShopState extends State
 			scene: scene,
 		});
 		
-		var window_y = 250;
+		var window_y = 150;
 		var window_w = 500;
 		var window_h = canvas.h - window_y;
 		var grid_padding = 10;
 
+		var panel_colour : Color = new Color();
+		panel_colour.rgb(0x000000);
+		panel_colour.a = 0;
+		
 		var grid_panel : Panel = new Panel({
 			parent: canvas,
             name: "panel",
-            options: { color:new Color(), color_bar:new Color().rgb(0xFFFFFF) },
+            options: { color:panel_colour },
             x: (canvas.w / 2) - (window_w / 2) + grid_padding, y:window_y + grid_padding, 
 			w:window_w - (grid_padding * 2), h: window_h,
 			mouse_input: true,
 		});
 
-		coins_text = new mint.Label({
+		coins_text = new MintLabel({
                 parent: grid_panel, name: "coins",
-				x:grid_panel.w/2 - 100, y:0, 
-				w:100, h:18, 
-				text_size: 16,
+				x:0, y:grid_padding, 
+				w:grid_panel.w, h:60, 
+				text_size: 36,
                 text: "coins",
+				color : Main.global_info.text_color,
+				align: TextAlign.center, align_vertical: TextAlign.center,
             });
 			
 		update_coins_text();
 			
 		var char_header : mint.Image = new mint.Image({
                 parent: grid_panel, name: "bgc",
-                x:0, y:coins_text.y_local + coins_text.h, 
+                x:0, y:coins_text.y_local + coins_text.h + grid_padding, 
 				w:grid_panel.w, h:80,
                 path: "assets/image/ui/unlockables_characters_button.png"
             });
 
 		var character_panel : MintGridPanel = new MintGridPanel(grid_panel, "Characters", 
-			new Vector(0, char_header.y_local + char_header.h), grid_panel.w, 5, 5, 1);		
+			new Vector(0, char_header.y_local + char_header.h + grid_padding), grid_panel.w, 5, 5, 1, panel_colour);		
 		load_character_grid(character_panel);
 
 		var background_header : mint.Image = new mint.Image({
                 parent: grid_panel, name: "bgh",
-                x:0, y:character_panel.y_local + character_panel.h, 
+                x:0, y:character_panel.y_local + character_panel.h + grid_padding, 
 				w:grid_panel.w, h:80,
                 path: "assets/image/ui/unlockables_environments_button.png"
             });
 		
 		var background_panel : MintGridPanel = new MintGridPanel(grid_panel, "Background", 
-			new Vector(0, background_header.y_local + background_header.h), grid_panel.w, 5, 5, 1);
+			new Vector(0, background_header.y_local + background_header.h + grid_padding), grid_panel.w, 5, 5, 1, panel_colour);
 		load_background_grid(background_panel);
 
 		//Reupdate here as we now know what size we are ^_^
@@ -169,31 +177,21 @@ class ShopState extends State
 	{
 		for (i in 0...Main.achievement_manager.character_groups.length)
 		{
+			//Item Button
 			var item : MintImageButton_Store = new MintImageButton_Store(character_panel, Main.achievement_manager.character_groups[i].name, 
 				new Vector(0, 0), new Vector(143, 193), 
 				Main.achievement_manager.character_groups[i].tex_path);
-			
-			var item_text : Label = new mint.Label({
-                parent: item, 
-				name: Main.achievement_manager.character_groups[i].name + "_coins",
-				x:item.x_local, y:item.y_local, 
-				w:10, h:18, 
-				text_size: 16,
-                text: "Cost: " + Main.achievement_manager.character_groups[i].cost,
-            });
+			character_panel.add_item(item);
 
-			
+			//On click action
 			item.onmouseup.listen(
 			function(e,c) 
 			{
 				trace("clicked!" + Main.achievement_manager.character_groups[i].name);
 				clicked_character(item, Main.achievement_manager.character_groups[i]);
 			});
-				
-			character_panel.add_item(item);
 			
-						
-				
+			//Initial update
 			var is_dirty = false;	
 			//Check if character was unlocked here as we don't update.
 			if (Main.achievement_manager.is_character_unlocked(Main.achievement_manager.character_groups[i].name))
@@ -201,6 +199,21 @@ class ShopState extends State
 				item.is_unlocked = true;
 				is_dirty = true;
 			}
+			else
+			{						
+				//Cost text
+				var item_text : MintLabel = new MintLabel({
+					parent: item, 
+					name: Main.achievement_manager.character_groups[i].name + "_coins",
+					x:0, y:0, 
+					w:item.w, h:item.h, 
+					text_size: 32,
+					text: Main.achievement_manager.character_groups[i].cost + "\n Coins",
+					color : new Color(),
+					align: TextAlign.center, align_vertical: TextAlign.center,
+				});
+			}
+			
 			if (Main.achievement_manager.selected_character == Main.achievement_manager.character_groups[i].name)
 			{
 				item.is_equipped = true;
@@ -211,7 +224,7 @@ class ShopState extends State
 			//Update the button!			
 			if (is_dirty)
 			{
-				//item.update_button();
+				item.update_button();
 			}
 		}
 	}
@@ -220,20 +233,15 @@ class ShopState extends State
 	{
 		for (i in 0...Main.achievement_manager.background_groups.length)
 		{
+			//Item button
 			var item : MintImageButton_Store = new MintImageButton_Store(background_panel, Main.achievement_manager.background_groups[i].name, 
 				new Vector(0, 0), new Vector(143, 193), 
 				Main.achievement_manager.background_groups[i].tex_path);
-			
-			var item_text : Label = new mint.Label({
-                parent: item, 
-				name: Main.achievement_manager.background_groups[i].name + "_coins",
-				x:item.w/2, y:item.h*0.9, 
-				w:10, h:18, 
-				text_size: 16,
-                text: "Cost: " + Main.achievement_manager.background_groups[i].cost,
-            });
+			background_panel.add_item(item);
 			
 			
+			
+			//On click action
 			item.onmouseup.listen(
 			function(e,c) 
 			{
@@ -241,9 +249,7 @@ class ShopState extends State
 				clicked_background(item, Main.achievement_manager.background_groups[i]);
 			});
 				
-			background_panel.add_item(item);
-			
-			
+			//Check state
 			var is_dirty = false;
 			//Check if character was unlocked here as we don't update.
 			if (Main.achievement_manager.is_background_unlocked(Main.achievement_manager.background_groups[i].name))
@@ -251,6 +257,21 @@ class ShopState extends State
 				item.is_unlocked = true;
 				is_dirty = true;
 			}
+			else
+			{
+				//Cost text.
+				var item_text : MintLabel = new MintLabel({
+					parent: item, 
+					name: Main.achievement_manager.background_groups[i].name + "_coins",
+					x:0, y:0, 
+					w:item.w, h:item.h, 
+					text_size: 32,
+					text: Main.achievement_manager.background_groups[i].cost + "\n Coins",
+					color : new Color(),
+					align: TextAlign.center, align_vertical: TextAlign.center,
+				});
+			}
+			
 			if (Main.achievement_manager.selected_background == Main.achievement_manager.background_groups[i].name)
 			{
 				item.is_equipped = true;
@@ -260,7 +281,7 @@ class ShopState extends State
 			
 			if (is_dirty)
 			{
-				//item.update_button();
+				item.update_button();
 			}
 		}
 	}
@@ -291,7 +312,7 @@ class ShopState extends State
 				button.is_unlocked = true;
 				Main.achievement_manager.unlock_character(character.name);
 				Main.achievement_manager.current_coins -= character.cost;
-				
+				button.destroy_children();
 				button.update_button();
 			}
 		}
@@ -326,7 +347,7 @@ class ShopState extends State
 				button.is_unlocked = true;
 				Main.achievement_manager.unlock_background(background.name);
 				Main.achievement_manager.current_coins -= background.cost;
-				
+				button.destroy_children();
 				button.update_button();
 			}
 		}
@@ -336,6 +357,6 @@ class ShopState extends State
 	
 	function update_coins_text()
 	{
-		coins_text.text = "Coins: " + Main.achievement_manager.current_coins;
+		coins_text.text = "Coins: " + Main.achievement_manager.current_coins + "\n Earn Coins by playing songs";
 	}
 }
