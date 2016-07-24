3eb415e17584c3cd2a73e5c9f7e964a4dbc787a8
aik6980 <aik6980>
2016-07-24 23:08:03 +0100

    bugfixing

--- gameluxe/src/gamestates/StoryCompleteState.hx
+++ gameluxe/src/gamestates/StoryCompleteState.hx
@@ -49,6 +49,30 @@ class StoryCompleteState extends State
 			scene: scene,
 			batcher: Main.batcher_ui,
 		});
+		
+		var click_to_cont = new Text({
+				font: Luxe.resources.font(Main.rise_font_id),
+				text: "Click to Continue...",
+				align: TextAlign.center,
+				align_vertical: TextAlign.center,
+				point_size: 36,
+				pos: new Vector(Main.mid_screen_pos().x, 50),
+				scene: scene,
+				color: new Color().rgb(0x3f2414),
+				outline: 0,
+				glow_amount: 0,
+				visible: true,
+				batcher: Main.batcher_ui,
+			});	
+		click_to_cont.color.a = 0;
+		
+		var fade_in_duration = 1.5;
+		var first_delay = 1.0;
+		Actuate.tween(click_to_cont.color, fade_in_duration, { a: 1.0 }).delay(first_delay + first_delay * 11).onComplete(
+			function()
+			{
+				Actuate.tween(click_to_cont.color, 2.6, { a: 0.1 }).repeat( -1).reflect();
+			});
 	}
 	
 	override function update(dt:Float) 
